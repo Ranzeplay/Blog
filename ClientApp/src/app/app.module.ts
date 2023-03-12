a5632c377dfca06b82629a1e2d1a537315a8c563
Jeb Feng <ranzeplay@outlook.com>
2023-03-12 11:25:33 +0800

    Removed default-generated functions

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ArticleReadComponent } from './article/read/read.component';
 import { ArticleViewComponent } from './article/view/view.component';
 import { GoTopButtonModule } from 'ng-go-top-button';
@@ -26,8 +24,6 @@ import { DetailComponent as TagDetailComponent } from './tag/detail/detail.compo
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ArticleReadComponent,
     ArticleViewComponent,
     ArticleListComponent,
@@ -53,8 +49,6 @@ import { DetailComponent as TagDetailComponent } from './tag/detail/detail.compo
       { path: 'category/detail/:id', component: CategoryDetailComponent },
       { path: 'tag/list', component: TagListComponent },
       { path: 'tag/detail/:id', component: TagDetailComponent },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
     ]),
   ],
   providers: [],
