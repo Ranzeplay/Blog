ce3c8482d5f6de814d781935b0585f19df663e1c
Jeb Feng <ranzeplay@outlook.com>
2023-03-26 12:00:14 +0800

    Add more information on home page

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { faGit, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { ArticleMetadata } from '../models/articleMetadata';
+import { CategoryIndex } from '../models/categoryIndex';
+import { TagIndex } from '../models/tagIndex';
+import { ArticleService } from '../services/article.service';
+import { CategoryService } from '../services/category.service';
 import { PageService } from '../services/page.service';
+import { TagService } from '../services/tag.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent{
+export class HomeComponent implements OnInit {
   faGitHub = faGithub;
   faEnvelope = faEnvelope;
   faYouTube = faYoutube;
@@ -15,4 +21,28 @@ export class HomeComponent{
   faGamePad = faGamepad;
 
   homePageId = '{{ home }}';
+
+  latestArticles: ArticleMetadata[] = [];
+  categories: CategoryIndex[] = [];
+  tags: TagIndex[] = [];
+
+  constructor(
+    private articleService: ArticleService,
+    private categoryService: CategoryService,
+    private tagService: TagService
+  ) {}
+
+  ngOnInit(): void {
+    this.articleService.indexArticles().subscribe(articles => {
+      this.latestArticles = articles;
+    });
+
+    this.categoryService.indexCategories().subscribe(categories => {
+      this.categories = categories;
+    });
+
+    this.tagService.indexTags().subscribe(tags => {
+      this.tags = tags;
+    });
+  }
 }
