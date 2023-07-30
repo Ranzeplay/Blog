160f7ac97493ea4f8fb4e1936cce7598d09daba1
Jeb Feng <ranzeplay@outlook.com>
2023-07-30 22:26:28 +0800

    Update About page

--- Client/src/app/full-screen-about/full-screen-about.component.ts
+++ Client/src/app/full-screen-about/full-screen-about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-full-screen-about',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./full-screen-about.component.css']
 })
 export class FullScreenAboutComponent {
-
+  arrowRight = faArrowRight;
 }
