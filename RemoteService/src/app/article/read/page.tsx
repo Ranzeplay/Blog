5a3913d0b63a31d6244c4b6663082e58e5ca7150
Jeb Feng <ranzeplay@outlook.com>
2023-12-17 14:59:29 +0800

    Improved the example reading page

--- RemoteService/src/app/article/read/page.tsx
+++ RemoteService/src/app/article/read/page.tsx
@@ -6,27 +6,38 @@ import rehypeReact from "rehype-react";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
+import { readingTime } from 'reading-time-estimator'
 
 export const metadata: Metadata = {
 	title: 'Read - Blog'
 };
 
 // @ts-expect-error: the react types are missing.
-const production = {Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs}
+const production = { Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs }
 
 export default async function Page() {
+	const articleMarkdown = await fetchArticle();
+	const timeToRead = readingTime(articleMarkdown);
+
 	const content = unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeReact, production)
-    .processSync(await fetchArticle()).result;
+		.use(remarkParse)
+		.use(remarkRehype)
+		.use(rehypeReact, production)
+		.processSync(articleMarkdown).result;
 
 	return (
-		<div className={`card`}>
-			<h1 className="font-serif font-bold text-2xl">Hello</h1>
+		<div className="card">
+			<div className="m-6">
+				<h1 className="font-serif font-bold text-4xl">Title</h1>
+				<div className="font-mono font-light text-gray-500">
+					<span>Publish time</span>
+					<span className="mx-2">|</span>
+					<span>{timeToRead.text}</span>
+				</div>
 
-			<div className={styles.content}>
-				{content}
+				<div className={styles.content + " mt-12"}>
+					{content}
+				</div>
 			</div>
 		</div>
 	)
