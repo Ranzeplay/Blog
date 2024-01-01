4201e8fb639dfcb94ee8bb28b483314e66f978e2
Jeb Feng <ranzeplay@outlook.com>
2024-01-01 15:47:14 +0800

    Implement responsive layout for whoami page

--- RemoteService/src/app/whoami/page.tsx
+++ RemoteService/src/app/whoami/page.tsx
@@ -17,24 +17,24 @@ export default function Page() {
 			<img className={styles.backgroundImg + " w-full h-screen bg-black brightness-50"} src="https://ranzeplay.space/assets/about/background.png" alt="Background image" />
 
 			<div className="backdrop-blur-sm">
-				<div className="flex relative h-screen w-5/6">
+				<div className="flex md:flex-row flex-col relative h-screen md:w-5/6">
 					<div className="flex flex-col w-full content-between items-between relative my-auto">
 						<img className="p-4 mx-auto w-2/5 aspect-auto rounded-full bg-white drop-shadow-xl outline-8 outline-gray-300" src="https://ranzeplay.space/assets/about/avatar.svg" alt="Jeb Feng's avatar" />
 					</div>
 
 					<div className="flex flex-col w-full content-between items-between relative my-auto">
-						<div className="items-end mr-16 ml-5">
+						<div className="items-end md:mr-16 md:ml-5 mx-8">
 							<h5 className="font-mono text-right text-white">$ whoami</h5>
 							<h2 className="text-right text-6xl font-bold title-text text-white drop-shadow-md">Jeb Feng</h2>
 
-							<div className="grid grid-cols-2 text-center gap-1 mt-8 font-mono ml-12 text-lg">
+							<div className="grid grid-cols-1 xl:grid-cols-2 text-center gap-1 mt-8 font-mono text-lg">
 								<div className={styles.myTag}>&lt;Developer /&gt;</div>
 								<div className={styles.myTag}>&lt;Senior High /&gt;</div>
 								<div className={styles.myTag}>&lt;INFP-T /&gt;</div>
 								<div className={styles.myTag}>&lt;Gen Z /&gt;</div>
 							</div>
 
-							<div className="flex flex-row-reverse text-xl mt-2 gap-x-4">
+							<div className="flex flex-row-reverse text-xl mt-2 justify-between xl:w-3/5 xl:ml-auto xl:mr-0">
 								<span className={styles.clickableIconWrapper}>
 									<Link className="mgc_mail_line" href={'mailto:ranzeplay@outlook.com'}></Link>
 								</span>
