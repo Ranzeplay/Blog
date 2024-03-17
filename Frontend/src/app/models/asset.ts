0e7e4c115632380a1735447f76c46784f1fdfe33
Jeb Feng <ranzeplay@outlook.com>
2024-03-17 11:42:56 +0800

    Begin migration from Next.js to Qwik for frontend project

--- Frontend/src/app/models/asset.ts
+++
@@ -1,7 +0,0 @@
-export class Asset {
-  constructor(
-    public name: string,
-    public blob: Blob,
-    public contentType: string
-  ) {}
-}
--- Frontend/src/app/projects/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-	return (
-		<div className="card">
-			<h2 className="font-serif text-2xl">Projects</h2>
-		</div>
-	)
-}
